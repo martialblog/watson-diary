3e833b03710f7d53bd6f71b903d2d00dcd3da1b1
Markus Opolka <markus@martialblog.de>
2017-06-09 22:56:26 +0200

    Better dashboard navigation

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,22 +1,70 @@
 <template>
   <v-app>
-    <v-navigation-drawer  persistent dark :mini-variant.sync="mini" v-model="drawer">
-      <v-card class="ma-4">
-        <v-list dense class="py-0">
-          <v-list-item v-for="item in items" :key="item">
-            <router-link :to="item.link" style="text-decoration:none;">
+    <v-navigation-drawer
+      class="pb-0"
+      height="100%"
+      persistent
+      dark
+      :mini-variant.sync="mini"
+      v-model="drawer"
+    >
+      <v-list dense>
+        <template v-for="(item, i) in items">
+          <v-layout
+            row
+            v-if="item.heading"
+            align-center
+            :key="i"
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+          </v-layout>
+          <v-list-group v-else-if="item.children" v-model="item.model" no-action>
+            <v-list-item slot="item">
               <v-list-tile>
                 <v-list-tile-action>
-                  <v-icon>{{ item.icon }}</v-icon>
+                  <v-icon>{{ item.model ? item.icon : item['icon-alt'] }}</v-icon>
                 </v-list-tile-action>
                 <v-list-tile-content>
-                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  <v-list-tile-title>
+                    {{ item.text }}
+                  </v-list-tile-title>
                 </v-list-tile-content>
               </v-list-tile>
-            </router-link>
+            </v-list-item>
+            <v-list-item
+              v-for="(child, i) in item.children"
+              :key="i"
+            >
+              <v-list-tile router :to="child.link">
+                <v-list-tile-action v-if="child.icon">
+                  <v-icon>{{ child.icon }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    {{ child.text }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item v-else>
+            <v-list-tile router :to="item.link">
+              <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
           </v-list-item>
-        </v-list>
-      </v-card>
+        </template>
+      </v-list>
     </v-navigation-drawer>
 
     <v-toolbar class="blue accent-4" light>
@@ -31,19 +79,38 @@
 
 <script>
   export default {
-    data () {
-      return {
-        drawer: true,
-        items: [
-          { title: 'Home', icon: 'dashboard', link: '/home'},
-          { title: 'Admin', icon: 'build', link: '/admin'},
-          { title: 'Profile', icon: 'account_circle', link: '/profile'},
-          { title: 'Logout', icon: 'exit_to_app', link: '/logout'}
+    data: () => ({
+      drawer: true,
+      items: [
+        { icon: 'dashboard', text: 'Home', link: '/home' },
+        {
+          icon: 'build',
+          'icon-alt': 'keyboard_arrow_down',
+          text: 'Admin',
+          model: true,
+          children: [
+            { text: 'Users', link: '/usermanager'},
+            { text: 'Reports', link: '/reportmanager'},
+            { text: 'Feeds', link: '/feedmanager'},
 
-        ],
-        right: null,
-      }
-    }
+          ]
+        },
+        {
+          icon: 'account_circle',
+          'icon-alt': 'keyboard_arrow_down',
+          text: 'Profile' ,
+          model: false,
+          children: [
+            { text: 'Diary', link: '/diary' },
+            { text: 'Manage Diary', link: '/diarymanager' },
+            { text: 'Manage Profile', link: '/profilemanager' },
+          ]
+        },
+        // { icon: 'settings', text: 'Settings' },
+        { icon: 'chat_bubble', text: 'Send feedback', link: '/feedback'},
+        { icon: 'help', text: 'Help', link: '/help' },
+      ]
+    })
   }
 </script>
 
